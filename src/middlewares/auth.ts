6f893813155ef4a68cd2b9161e72e7e45cc68612
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2025-12-28 20:50:20 +0700

    refresh token rotation

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,4 +1,7 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { errorCode } from "../config/errorCode";
+import { getUserById, updateUser } from "../services/auth";
 
 interface CustomRequest extends Request {
   userId?: number;
@@ -14,11 +17,130 @@ export const auth = (req: CustomRequest, res: Response, next: NextFunction) => {
     return next(err);
   }
 
+  const generateNewTokens = async () => {
+    let decoded;
+    try {
+      decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!) as {
+        id: number;
+        phone: string;
+      };
+    } catch (error) {
+      // return next(
+      //   createError(
+      //     "You are not an authenticated user.",
+      //     401,
+      //     errorCode.unauthenticated
+      //   )
+      // );
+      const err: any = new Error("You are not an authenticated user.");
+      err.status = 401;
+      err.code = errorCode.unauthenticated;
+      return next(err);
+    }
+
+    if (isNaN(decoded.id)) {
+      const err: any = new Error("You are not an authenticated user.");
+      err.status = 401;
+      err.code = errorCode.unauthenticated;
+      return next(err);
+    }
+
+    const user = await getUserById(decoded.id);
+    if (!user) {
+      const err: any = new Error("You are not an authenticated user.");
+      err.status = 401;
+      err.code = errorCode.unauthenticated;
+      return next(err);
+    }
+
+    if (user.phone !== decoded.phone) {
+      const err: any = new Error("You are not an authenticated user.");
+      err.status = 401;
+      err.code = errorCode.unauthenticated;
+      return next(err);
+    }
+
+    if (user.randToken !== refreshToken) {
+      const err: any = new Error("You are not an authenticated user.");
+      err.status = 401;
+      err.code = errorCode.unauthenticated;
+      return next(err);
+    }
+
+    // Authorization token
+    const accessTokenPayload = { id: user.id };
+    const refreshTokenPayload = { id: user.id, phone: user.phone };
+
+    const newAccessToken = jwt.sign(
+      accessTokenPayload,
+      process.env.ACCESS_TOKEN_SECRET!,
+      {
+        expiresIn: 60 * 15, // 15 min
+      }
+    );
+
+    const newRefreshToken = jwt.sign(
+      refreshTokenPayload,
+      process.env.REFRESH_TOKEN_SECRET!,
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const userData = {
+      randToken: newRefreshToken,
+    };
+
+    await updateUser(user.id, userData);
+
+    res
+      .cookie("accessToken", newAccessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 15 * 60 * 1000, // 15 minutes
+      })
+      .cookie("refreshToken", newRefreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      });
+
+    req.userId = user.id;
+    next();
+  };
+
   if (!accessToken) {
-    const err: any = new Error("Access token is expired");
-    err.status = 401;
-    err.code = "Access_Token_Expired";
-    return next(err);
+    return generateNewTokens();
+  } else {
+    let decoded;
+    try {
+      decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET!) as {
+        id: number;
+      };
+
+      if (isNaN(decoded.id)) {
+        const err: any = new Error("You are not an authenticated user.");
+        err.status = 401;
+        err.code = errorCode.unauthenticated;
+        return next(err);
+      }
+
+      req.userId = decoded.id;
+      next();
+    } catch (error: any) {
+      if (error.name === "TokenExpiredError") {
+        generateNewTokens(); // await generateNewTokens();
+        // error.message = "Access Token has expired.";
+        // error.status = 401;
+        // error.code = errorCode.accessTokenExpired;
+      } else {
+        error.message = "Access Token is invalid.";
+        error.status = 400;
+        error.code = errorCode.attack;
+        return next(error);
+      }
+    }
   }
-  next();
 };
