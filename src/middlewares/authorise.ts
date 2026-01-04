10e07348f8952cea4d20daf25ddc70f88d68f9ca
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2026-01-04 15:13:21 +0700

    authorise finish

--- src/middlewares/authorise.ts
+++ src/middlewares/authorise.ts
@@ -1,11 +1,40 @@
 import { Request, Response, NextFunction } from "express";
+import { errorCode } from "../config/errorCode";
+import { getUserById } from "../services/auth";
 interface CustomRequest extends Request {
   userId?: number;
+  user?: any;
 }
-export const authorise = (
-  req: CustomRequest,
-  res: Response,
-  next: NextFunction
-) => {
-  next();
+export const authorise = (permission: boolean, ...roles: string[]) => {
+  return async (req: CustomRequest, res: Response, next: NextFunction) => {
+    const userId = req.userId;
+    const user = await getUserById(userId!);
+    if (!user) {
+      const err: any = new Error("This account is not registered.");
+      err.status = 404;
+      err.code = errorCode.unauthenticated;
+      return next(err);
+    }
+
+    const result = roles.includes(user.role);
+
+    // permission && result
+
+    if (permission && !result) {
+      const err: any = new Error("This action is not allowed.");
+      err.status = 403;
+      err.code = errorCode.unauthorised;
+      return next(err);
+    }
+
+    if (!permission && result) {
+      const err: any = new Error("This action is not allowed.");
+      err.status = 403;
+      err.code = errorCode.unauthorised;
+      return next(err);
+    }
+
+    req.user = user;
+    next();
+  };
 };
