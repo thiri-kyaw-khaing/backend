5a129c434ad24ccab9605990be5e8e542ebe49d1
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2026-01-11 15:42:35 +0700

    error refactor

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,1 +1,6 @@
-export const;
+export const createError = (message: string, status: number, code: string) => {
+  const error: any = new Error(message);
+  error.status = status;
+  error.code = code;
+  return error;
+};
