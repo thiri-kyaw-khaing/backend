8030d7da71598e42a8ae4c6c765b8cb73e3e11a8
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2026-01-28 18:55:23 +0700

    queue and worker

---
+++ src/jobs/workers/imageWorker.ts
@@ -0,0 +1,39 @@
+import { Worker } from "bullmq";
+import sharp, { concurrency } from "sharp";
+import path from "path";
+
+// import { redis } from "../../../config/redisClient";
+
+// Create a worker to process the image optimization job
+const imageWorker = new Worker(
+  "imageQueue",
+  async (job) => {
+    const { filePath, fileName, width, height, quality } = job.data;
+
+    const optimizedImagePath = path.join(
+      __dirname,
+      "../../..",
+      "/uploads/optimize",
+      fileName,
+    );
+    await sharp(filePath)
+      .resize(width, height)
+      .webp({ quality: quality })
+      .toFile(optimizedImagePath);
+  },
+  //{ connection: redis }
+  {
+    connection: {
+      host: process.env.REDIS_HOST || "172.0.0.1",
+      port: Number(process.env.REDIS_PORT!) || 6379,
+    },
+  },
+);
+
+imageWorker.on("completed", (job) => {
+  console.log(`Job completed with result ${job.id}`);
+});
+
+imageWorker.on("failed", (job: any, err) => {
+  console.log(`Job ${job.id} failed with ${err.message}`);
+});
