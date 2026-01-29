71101a7ecc4f99ba3e84875c236fa7c6eb201dcf
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2026-01-29 16:03:28 +0700

    postController for admin

---
+++ src/controllers/admin/postController.ts
@@ -0,0 +1,68 @@
+import { body, check, validationResult } from "express-validator";
+import { NextFunction, Request, Response } from "express";
+
+export const createPost = [
+  body("phone", "Phone number is not valid")
+    .trim()
+    .notEmpty()
+    .matches("^[0-9]+$")
+    .isLength({ min: 5, max: 12 })
+    .withMessage("Phone number must be between 5 to 12 digits"),
+  async (req: Request, res: Response, next: NextFunction) => {
+    // Registration logic here
+    const errors = validationResult(req).array({ onlyFirstError: true });
+    //if vlidation error occurs
+    if (errors.length > 0) {
+      const error: any = new Error(errors[0].msg);
+      error.status = 400;
+      error.code = "Error_Invalid";
+      return next(error);
+    }
+    let phone = req.body.phone; //09123456789
+    res.status(201).json({ message: "Post created successfully" });
+  },
+];
+
+export const updatePost = [
+  body("phone", "Phone number is not valid")
+    .trim()
+    .notEmpty()
+    .matches("^[0-9]+$")
+    .isLength({ min: 5, max: 12 })
+    .withMessage("Phone number must be between 5 to 12 digits"),
+  async (req: Request, res: Response, next: NextFunction) => {
+    // Registration logic here
+    const errors = validationResult(req).array({ onlyFirstError: true });
+    //if vlidation error occurs
+    if (errors.length > 0) {
+      const error: any = new Error(errors[0].msg);
+      error.status = 400;
+      error.code = "Error_Invalid";
+      return next(error);
+    }
+    let phone = req.body.phone; //09123456789
+    res.status(201).json({ message: "Post created successfully" });
+  },
+];
+
+export const deletePost = [
+  body("phone", "Phone number is not valid")
+    .trim()
+    .notEmpty()
+    .matches("^[0-9]+$")
+    .isLength({ min: 5, max: 12 })
+    .withMessage("Phone number must be between 5 to 12 digits"),
+  async (req: Request, res: Response, next: NextFunction) => {
+    // Registration logic here
+    const errors = validationResult(req).array({ onlyFirstError: true });
+    //if vlidation error occurs
+    if (errors.length > 0) {
+      const error: any = new Error(errors[0].msg);
+      error.status = 400;
+      error.code = "Error_Invalid";
+      return next(error);
+    }
+    let phone = req.body.phone; //09123456789
+    res.status(201).json({ message: "Post created successfully" });
+  },
+];
--- src/routes/v1/admin/index.ts
+++ src/routes/v1/admin/index.ts
@@ -2,8 +2,18 @@ import express from "express";
 
 import { getAllUsers } from "../../../controllers/admin/userController";
 import { auth } from "../../../middlewares/auth";
+import upload from "../../../middlewares/uploadeFile";
+import {
+  createPost,
+  deletePost,
+  updatePost,
+} from "../../../controllers/admin/postController";
 const router = express.Router();
 
 router.get("/users", auth, getAllUsers);
+
+router.post("/posts", upload.single("image"), createPost);
+router.patch("/posts", upload.single("image"), updatePost);
+router.delete("posts", deletePost);
 
 export default router;
